64201f7b231bfe837c50bdfefef28b36287dfba3
Trueno110 <Trueno110@yandex.ru>
2024-04-11 12:00:50 +0300

    Добавьте файлы проекта.

---
+++ NuChoTi_Chati_Chati_ili_Narmaldaki/Enterence.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NuChoTi_Chati_Chati_ili_Narmaldaki
+{
+    /// <summary>
+    /// Логика взаимодействия для Enterence.xaml
+    /// </summary>
+    public partial class Enterence : Window
+    {
+        public Enterence()
+        {
+            InitializeComponent();
+        }
+
+        private void BXOD_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+
+        private void Zaregaca_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NuChoTi_Chati_Chati_ili_Narmaldaki/UserSet.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NuChoTi_Chati_Chati_ili_Narmaldaki
+{
+    /// <summary>
+    /// Логика взаимодействия для UserSet.xaml
+    /// </summary>
+    public partial class UserSet : Window
+    {
+        public UserSet()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void users_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
